36d184347ddbe3709fdc59a97e8a9d647fbc9ff0
ArchLeaders <archleadership28@gmail.com>
2024-08-31 14:25:45 -0700

    Add sample files

---
+++ RecreateMarkdownRenderErrors/Views/MainView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+
+namespace RecreateMarkdownRenderErrors.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        MarkdownViewer.Markdown = "Test";
+    }
+}
